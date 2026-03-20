7e262489315a977ff03884ca7385d5f0ce48e3f3
Christian Bermudez <chrisb7560@gmail.com>
2026-03-19 17:12:39 -0700

    exercise 1 completion

--- js/ex1.js
+++ js/ex1.js
@@ -30,3 +30,27 @@ const getCharacters = houseCode => {
             return[]; // Empty array
     }
 };
+
+// Fill the dropdown on page load
+const select = document.getElementById("house");
+
+houses.forEach(house => {
+    const option = document.createElement("option");
+    option.value = house.code;
+    option.textContent = house.name;
+    select.appendChild(option);
+});
+
+// Show characters when user selects a house
+select.addEventListener("change", () => {
+    const characters = getCharacters(select.value);
+    const ul = document.getElementById("characters");
+
+    ul.innerHTML = ""; // Clear previous list
+
+    characters.forEach(name => {
+        const li = document.createElement("li");
+        li.textContent = name;
+        ul.appendChild(li);
+    });
+});
